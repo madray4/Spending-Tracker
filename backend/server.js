dc5e4c8603d106c8082e4a7379b55810f0aeff09
Henry Chu <henry.chu013198@gmail.com>
2023-04-30 22:54:52 -0400

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose');
 const entryRoutes = require('./routes/entryRoutes');
 
 const app = express();
 
 // middleware
 app.use(express.json());
+
 app.use((req, res, next) => {
   console.log('~~~ Request Type: ' + req.method + ', Path: ' + req.path);
   next();
@@ -15,7 +17,14 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/entries', entryRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('~~~ Server listening on port: ' + process.env.PORT);
-});
+// connect to database & listen for requests 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('~~~ Connected to database');
+    app.listen(process.env.PORT, () => {
+      console.log('~~~ Server listening on port: ' + process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log('~~~ ' + error.message);
+  })
