2ef974857cc3677657ef83cda25a0bb0026848bb
Henry Chu <henry.chu013198@gmail.com>
2023-05-01 10:40:35 -0400

    CRUD functionality for Entries

---
+++ backend/controllers/entryController.js
@@ -0,0 +1,72 @@
+const Entry = require ('../models/entryModel');
+const mongoose = require ('mongoose');
+
+// GET all entries
+const getEntries = async (req, res) => {
+  const entries = await Entry.find({});
+  res.status(200).json(entries);
+} ;
+
+// GET a single entry
+const getEntry = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(500).json({error: 'No such entry'});
+  }
+
+  const entry = await Entry.findById(id);
+  if(!entry) {
+    return res.status(500).json({error: 'No such entry'});
+  }
+  res.status(200).json(entry);
+}
+
+// CREATE an entry
+const createEntry = async (req, res) => {
+  try{
+    const entry = await Entry.create({...req.body});
+    res.status(200).json(entry);
+  }
+  catch (error){
+    res.status(500).json({ error: error.message })
+  };
+};
+
+// DELETE an entry
+const deleteEntry = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(500).json({error: 'No such entry'});
+  }
+
+  const entry = await Entry.findByIdAndDelete(id);
+  if(!entry){
+    return res.status(500).json({error: 'No such entry'});
+  }
+  res.status(200).json(entry)
+};
+
+// UPDATE an entry
+const updateEntry = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(500).json({error: 'No such entry'});
+  }
+
+  const entry = await Entry.findByIdAndUpdate(id, {...req.body});
+  if(!entry){
+    return res.status(500).json({error: 'No such entry'});
+  }
+  res.status(200).json(entry);
+};
+
+module.exports = {
+  getEntries,
+  getEntry,
+  createEntry,
+  deleteEntry,
+  updateEntry
+};
--- backend/routes/entryRoutes.js
+++ backend/routes/entryRoutes.js
@@ -1,39 +1,27 @@
 const express = require('express');
-const Entry = require('../models/entryModel');
+const {
+  getEntries,
+  getEntry,
+  createEntry,
+  deleteEntry,
+  updateEntry
+} = require ('../controllers/entryController');
 
 const router = express.Router();
 
 // GET all entries
-router.get('/', (req, res) => {
-  res.status(200).json({mssg: 'GET all entries'});
-});
+router.get('/', getEntries);
 
 // GET a single entry
-router.get('/:id', (req, res) => {
-  res.status(200).json({mssg: 'GET an entry, id: ' + req.params.id});
-});
+router.get('/:id', getEntry);
 
 // CREATE an entry
-router.post('/', async (req, res) => {
-  const { store, item, totalCost, subTotal, shipping, tax, month, day, year, isNecessary } = req.body;
-
-  try{
-    const entry = await Entry.create({ store, item, totalCost, subTotal, shipping, tax, month, day, year, isNecessary});
-    res.status(200).json(entry);
-  }
-  catch (error){
-    res.status(500).json({ error: error.message })
-  };
-});
+router.post('/', createEntry);
 
 // DELETE an entry
-router.delete('/:id', (req, res) => {
-  res.status(200).json({mssg: 'DELETE an entry, id: ' + req.params.id});
-});
+router.delete('/:id', deleteEntry);
 
 // UPDATE an entry
-router.patch('/:id', (req, res) => {
-  res.status(200).json({mssg: 'UPDATE an entry, id: ' + req.params.id});
-});
+router.patch('/:id', updateEntry);
 
 module.exports = router;
