484f8c725013d74783b69a23c4fe0caa295bfc90
Henry Chu <henry.chu013198@gmail.com>
2023-05-06 20:49:18 -0400

    Added signup functionality on backend + password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,4 @@
 const User = require('../models/userModel');
-// const mongoose = require('mongoose');
 
 // login user
 const loginUser = async (req, res) => {
@@ -8,7 +7,14 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
-  res.json({mssg: 'signup user'});
-}
+  const { email, password } = req.body;
+  
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({email, user});
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+};
 
 module.exports = { loginUser, signupUser} ;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -14,4 +15,18 @@ const userSchema = new Schema({
   }
 });
 
-exports.modules = mongoose.model('User', userSchema);
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error('Email already in use');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+}
+
+module.exports = mongoose.model('User', userSchema);
