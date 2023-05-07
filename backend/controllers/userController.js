9e5cfabdf3dee027b64fb022ffa55a66be434942
Henry Chu <henry.chu013198@gmail.com>
2023-05-07 19:10:23 -0400

    Added email & strong password validation serverside

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,18 +3,17 @@ const User = require('../models/userModel');
 // login user
 const loginUser = async (req, res) => {
   res.json({mssg: 'login user'});
-}
+};
 
 // signup user
 const signupUser = async (req, res) => {
-  const { email, password } = req.body;
-  
+  const {email, password} = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({email, user});
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({error: error.message});
   }
 };
 
-module.exports = { loginUser, signupUser} ;
+module.exports = { loginUser, signupUser };
